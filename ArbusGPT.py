3bc759ff2e28d62bce379fdf1c5cf4ef59222372
Javier Amado <JavierAmado7@gmail.com>
2023-03-26 20:07:34 +0200

    Several changes
    Added requirements, updated readme, continuity in conversations, control of administrator users and OpenAI configurations are modified

--- ArbusGPT.py
+++ ArbusGPT.py
@@ -2,10 +2,12 @@ import os
 import openai
 import discord
 from discord import app_commands, Intents, Client, Interaction
+from dotenv import load_dotenv
 
 # Configura tus tokens de API
-TOKEN_DISCORD = os.environ['TOKEN_DISCORD']
-TOKEN_OPENAI = os.environ['TOKEN_OPENAI']
+load_dotenv()
+TOKEN_DISCORD = os.getenv('TOKEN_DISCORD')
+TOKEN_OPENAI = os.getenv('TOKEN_OPENAI')
 
 # Define los intents necesarios
 intents = discord.Intents.default()
@@ -17,25 +19,44 @@ class ArbusGPTBot(Client):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
         openai.api_key = TOKEN_OPENAI
+        self.conversaciones = {}
 
     async def on_ready(self):
         print(f'{bot.user} se ha conectado a Discord!')
         await self.tree.sync(guild=None)
 
+    async def es_administrador(self, miembro):
+        return miembro.guild_permissions.administrator
+
     async def chat_with_gpt3(self, interaction: Interaction, mensaje: str):
         model_engine = "text-davinci-002"
-        prompt = f'Conversación en Español:\n\nUsuario: {mensaje}\nIA:'
+        user_id = interaction.user.id
+        miembro = interaction.guild.get_member(user_id)
+
+        if "comando_administrador" in mensaje.lower():
+            if not await self.es_administrador(miembro):
+                await interaction.response.send_message("No tienes permisos de administrador para ejecutar este comando.")
+                return
+
+        if user_id not in self.conversaciones:
+            self.conversaciones[user_id] = []
+
+        self.conversaciones[user_id].append(f'Usuario: {mensaje}')
+
+        prompt = 'Conversación en Español:\n\n'
+        prompt += '\n'.join(self.conversaciones[user_id]) + '\nIA:'
 
         response = openai.Completion.create(
             engine=model_engine,
             prompt=prompt,
-            max_tokens=150,
+            max_tokens=1000,
             n=1,
             stop=None,
-            temperature=0.7,
+            temperature=1.0,
         )
 
         answer = response.choices[0].text.strip()
+        self.conversaciones[user_id].append(f'IA: {answer}')
         await interaction.response.send_message(answer)
 
 bot = ArbusGPTBot(intents=intents)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ArbusGPT
+
+## Librerías necesarias
+- pip install openai
+- pip install python-dotenv
+- pip install discord.py
